fd98a659723f59685990919f4fa217bf6cce42f4
Christian <Christian@192.168.0.15>
2015-12-14 07:54:11 +0100

    SHUTDOWN verwenden, wenns hart wegkackt

--- src/srpTracking/DatabaseConnector.java
+++ src/srpTracking/DatabaseConnector.java
@@ -1,8 +1,8 @@
 package srpTracking;
 
 import java.sql.*;
-import java.util.Arrays;
-
+import org.h2.*;
+import org.h2.tools.DeleteDbFiles;
 
 public class DatabaseConnector {
 
@@ -11,19 +11,12 @@ public class DatabaseConnector {
 		Class.forName("org.h2.Driver");
 		Connection conn = DriverManager.getConnection("jdbc:h2:C:/Program Files(x86)/H2", "Kaps", "");
 		Statement stat = conn.createStatement();
-
-		/* Old execution code, kept as an example
-		 * stat.execute("insert into test values(3, 'Penis')");
-		* ResultSet rs;
-		*rs = stat.executeQuery("SELECT * FROM unterricht");
-		*while (rs.next()) {
-		*	System.out.println(rs.getString("ID"));
-		}*/
+		
 		stat.close();
 		conn.close();
 	}
 
-		//insert lessons into UNTERRICHT tabel function
+	// insert lessons into UNTERRICHT tabel function
 	public void insert(String Eintrag) throws ClassNotFoundException, SQLException {
 		// Get Connection
 		Class.forName("org.h2.Driver");
@@ -42,36 +35,70 @@ public class DatabaseConnector {
 		stat.close();
 		conn.close();
 	}
-	public String[][] retrieveForTable() throws ClassNotFoundException, SQLException{
-		
+
+	public String[][] retrieveForTable() throws ClassNotFoundException, SQLException {
+
 		// Get Connection
 		Class.forName("org.h2.Driver");
 		Connection conn = DriverManager.getConnection("jdbc:h2:C:/Program Files(x86)/H2", "Kaps", "");
-		
-		//scroll insenstive, so you can scroll back and forth
-		Statement stat = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+		// scroll insenstive, so you can scroll back and forth
+		Statement stat = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 		ResultSet rs;
-		
-		//Query the last 3 additions
+
+		// Query the last 3 additions
 		rs = stat.executeQuery("SELECT * FROM unterricht ORDER BY id DESC LIMIT 3");
 		int count = 0;
-		while(rs.next()){
+		while (rs.next()) {
 			++count;
 		}
-		//Reset cursor to before first row
+		// Reset cursor to before first row
 		rs.beforeFirst();
-		String [][] queryErgebniss = new String [count][rs.getMetaData().getColumnCount()];
+		String[][] queryErgebniss = new String[count][rs.getMetaData().getColumnCount()];
 		int r = 0;
-		while(rs.next()){
-			for (int c=1;c<rs.getMetaData().getColumnCount()+1;c++){
-				queryErgebniss[r][c-1]= rs.getString(c);
+		while (rs.next()) {
+			for (int c = 1; c < rs.getMetaData().getColumnCount() + 1; c++) {
+				queryErgebniss[r][c - 1] = rs.getString(c);
 			}
 			++r;
 		}
 		rs.close();
 		stat.close();
 		conn.close();
-		
+
 		return queryErgebniss;
 	}
+
+	/*public String[] queryresultsfordropdown()
+			throws ClassNotFoundException, SQLException {
+		// Get Connection
+		Class.forName("org.h2.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:h2:C:/Program Files(x86)/H2", "Kaps", "");
+
+		// scroll insenstive, so you can scroll back and forth
+		Statement stat = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		ResultSet rs;
+
+		// Query the all last names of teachers
+		rs = stat.executeQuery("SELECT nachname FROM lehrer ORDER BY nachname ASC");
+
+		int count = 0;
+		while (rs.next()) {
+			++count;
+		}
+		// Reset cursor to before first row
+		rs.beforeFirst();
+		// Enter all Teachers, into one String
+		String[] queryErgebniss = new String[count];
+		int i = 0;
+		while (rs.next()) {
+			queryErgebniss[i] = rs.getString(columnname);
+			++i;
+		}
+		rs.close();
+		stat.close();
+		conn.close();
+
+		return queryErgebniss;
+	}*/
 }
